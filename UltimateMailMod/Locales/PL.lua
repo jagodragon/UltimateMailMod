104a67501973793dd3dde0bb93c03e8a143e1068
Jago <rphillip@live.com>
2014-08-18 23:15:41 -0400

    Added Polish and Italian
    Added Polish and italian languages to the addon. (may need to be cleaned
    up)

---
+++ UltimateMailMod/Locales/PL.lua
@@ -0,0 +1,178 @@
+
+-- #############################################################################
+-- ##                                                                         ##
+-- ##  Language     : Polish (PL)                                             ##
+-- ##  Locale       : PL                                                      ##
+-- ##  Last updated : 18 aug 2014                                             ##
+-- ##  Origin       : Translated                          				      ##
+-- ##  Author       : Jagodragon (PL)                                         ##
+-- ##  Credits      : Jagodragon (EN/EU)                                      ##
+-- ##                                                                         ##
+-- #############################################################################
+
+-- Generale
+UMM_TITLE                           = "Ostatecznym Poczta Mod";
+UMM_PROMPT                          = "UltimateMailMod";
+UMM_LOADED                          = " załadowany.";
+
+-- Slash settings
+UMM_SLASH_HELP1                     = "Pomoc dla poleceń:";
+UMM_SLASH_HELP2                     = "/umm sound : ostrzeżenie audio, przełączanie na/off.";
+UMM_SLASH_HELP3                     = "/umm reset : resetuje listę własnych bohaterów.";
+UMM_SLASH_AUDIODISABLED             = "Ostrzeżenie audio podczas odbierania nowych wiadomości jest teraz wyłączony.";
+UMM_SLASH_AUDIOENABLED              = "Ostrzeżenie audio podczas odbierania nowych wiadomości jest teraz aktywna.";
+UMM_SLASH_CHARSRESET                = "Lista własnych bohaterów zostało zresetowane.";
+
+-- New mail notify
+UMM_NOTIFY_NEWMAILARRIVED           = "Nadeszła nowa poczta";
+UMM_NOTIFY_TOOLTIP_TITLE            = "Nowa poczta";
+UMM_NOTIFY_TOOLTIP_NEWMAIL          = "Masz nowy mail, oczekujące.";
+UMM_NOTIFY_TOOLTIP_NEWMAILS         = "Masz %d nowy mail, oczekujące.";
+UMM_NOTIFY_TOOLTIP_MOVETIP          = "Shift + prawy myszki: przenieść TE ikonę.";
+
+-- Menu tabs
+UMM_MENU_TAB1                       = MAIL_INBOX; -- Domyślnie gry ciąg
+UMM_MENU_TAB2                       = MAIL_SENTMAIL; -- Domyślnie gry ciąg
+UMM_MENU_TAB3                       = "Msza Wyślij przedmioty";
+
+-- Tooltip specific
+UMM_TOOLTIP_BOUND                   = "Uwiązany";
+
+-- Setting
+UMM_SETTINGS_AUDIOWARNING           = "Odtwórz dźwięk, gdy otrzymują nowe maile.";
+
+-- Inbox
+UMM_HELP_INBOX_LINE1                = "Możesz wielokrotnego wyboru maile do :";
+UMM_HELP_INBOX_LINE2                = "1) Weź przedmioty / pieniądze / diamenty.";
+UMM_HELP_INBOX_LINE3                = "2) Masowe maile zwrotne.";
+UMM_HELP_INBOX_LINE4                = "3) Msza Usuń wiadomości.";
+UMM_HELP_INBOX_LINE5                = "Ctrl + lewy przycisk myszy, aby oznaczyć / odznaczyć mailu.";
+UMM_HELP_INBOX_RETURNTAGGED         = "Zwraca wszystkie zaznaczone wiadomości.";
+UMM_HELP_INBOX_DELETETAGGED         = "Usuwa wszystkie oznaczone maile.";
+
+UMM_INBOX_LOADING                   = "Ładowanie skrzynkę - proszę czekać ...";
+UMM_INBOX_EMPTY                     = "Twoja skrzynka odbiorcza jest pusta.";
+
+UMM_INBOX_BUTTON_TAGCHARS           = "bohaterów";
+UMM_INBOX_BUTTON_TAGFRIENDS         = "Znajomi";
+UMM_INBOX_BUTTON_TAGGUILDIES        = "koledzy gildia";
+UMM_INBOX_BUTTON_TAGOTHER           = "Inne";
+UMM_INBOX_BUTTON_TAGEMPTY           = "pusty";
+UMM_INBOX_BUTTON_TAKE               = "Weź";
+UMM_INBOX_BUTTON_RETURN             = "Powrót";
+UMM_INBOX_BUTTON_DELETE             = "Usuń";
+
+UMM_INBOX_OPTION_1                  = "Wszystko.";
+UMM_INBOX_OPTION_2                  = "Produkty.";
+UMM_INBOX_OPTION_3                  = "pieniądze.";
+UMM_INBOX_OPTION_4                  = "diamenty.";
+
+UMM_INBOX_CHECK_TOOLTIP				= "Włącz wyświetlanie podpowiedzi.";
+UMM_INBOX_CHECK_DELETEDONE          = "Usuwanie wiadomości po zakończeniu pobierania.";
+
+UMM_INBOX_LABEL_MASSTAG             = "Masa tagu kolczugi:";
+UMM_INBOX_LABEL_TOTALMONEY          = "Całkowitej ceny w skrzynce odbiorczej:";
+UMM_INBOX_LABEL_TOTALDIAMONDS       = "Całkowita diamentów w skrzynce odbiorczej:";
+
+UMM_INBOX_STATUS_TAKEALLTAGGED      = "Automatyczne otwieranie wiadomości - proszę czekać ...";
+UMM_INBOX_STATUS_PREPARETAKEDELETE  = "Przygotowanie do usuwanie maili otwarty - proszę czekać ...";
+UMM_INBOX_STATUS_RETURNTAGGED       = "Powracający automatycznie maile - proszę czekać ...";
+UMM_INBOX_STATUS_DELETETAGGED       = "Automatyczne usuwanie wiadomości - proszę czekać ...";
+
+--Przeglądarka
+UMM_VIEWER_LABEL_FROM 				= "od:";
+UMM_VIEWER_LABEL_SUBJECT 			= "temat:";
+
+UMM_VIEWER_BUTTON_REPLY 			= "Odpowiedzi";
+UMM_VIEWER_BUTTON_RETURN 			= "Powrót";
+UMM_VIEWER_BUTTON_DELETE 			= "Usuń";
+UMM_VIEWER_BUTTON_CLOSE 			= "Zamknięcie";
+
+UMM_VIEWER_ATTACHED 				= "dołączone:";
+UMM_VIEWER_ATTACHMENTCOD 			= "płatne przy odbiorze cena:";
+UMM_VIEWER_ATTACHMENTACCEPT		    = "Akceptuj płatne przy odbiorze";
+UMM_VIEWER_ATTACHMENT_NOT_ACCEPTED 	= "Proszę sprawdzić akceptuje COD i spróbuj ponownie.";
+
+--Kompozytor
+UMM_COMPOSER_ADDRESSEE 				= "do:";
+UMM_COMPOSER_SUBJECT	 			= "temat:";
+UMM_COMPOSER_BUTTON_RESET 			= "Zresetować";
+UMM_COMPOSER_BUTTON_SEND 			= "Wyślij";
+UMM_COMPOSER_ATTACHMENT 			= "zajęcia:";
+UMM_COMPOSER_AUTOSUBJECT 			= "pieniędzy:";
+UMM_COMPOSER_SENDGOLD 				= "Wyślij złota";
+UMM_COMPOSER_SENDCOD 				= "PŁATNE PRZY ODBIORZE";
+
+UMM_COMPOSER_CONFIRM_TEXT1 = "Czy na pewno chcesz wysłać kwota";
+UMM_COMPOSER_CONFIRM_TEXT2 = "pokazano poniżej na %s?";
+UMM_COMPOSER_CONFIRM_YES = "Yes";
+UMM_COMPOSER_CONFIRM_NO = "Nie";
+
+UMM_RECIPIENT_OWN = "Znaków";
+UMM_RECIPIENT_FRIEND = "Przyjaciół";
+UMM_RECIPIENT_GUILD = "Guildies";
+
+--Masowego wysyłania przedmiotów
+UMM_MSI_MARKBUTTON1 = "Run";
+UMM_MSI_MARKBUTTON2 = "F. kamienie";
+UMM_MSI_MARKBUTTON3 = "Juwels";
+UMM_MSI_MARKBUTTON4 = "Rud";
+UMM_MSI_MARKBUTTON5 = "Drewno";
+UMM_MSI_MARKBUTTON6 = "Zioła";
+UMM_MSI_MARKBUTTON7 = "R. maty.";
+UMM_MSI_MARKBUTTON8 = "P. runy";
+UMM_MSI_MARKBUTTON9 = "Żywności";
+UMM_MSI_MARKBUTTON10 = "Desery";
+UMM_MSI_MARKBUTTON11 = "Mikstury";
+UMM_MSI_MARKBUTTON12 = "Biały Rec.";
+UMM_MSI_MARKBUTTON13 = "Zielony Rec.";
+UMM_MSI_MARKBUTTON14 = "Niebieski Rec.";
+UMM_MSI_MARKBUTTON15 = "Magenta Rec.";
+UMM_MSI_MARKBUTTON16 = "Gildia";
+
+UMM_MSI_MARK_LABEL = "znak:";
+UMM_MSI_MARK_TOOLTIP1 = "Run";
+UMM_MSI_MARK_TOOLTIP2 = "Kamienie fuzji";
+UMM_MSI_MARK_TOOLTIP3 = "Juwels";
+UMM_MSI_MARK_TOOLTIP4 = "Rud";
+UMM_MSI_MARK_TOOLTIP5 = "Drewno";
+UMM_MSI_MARK_TOOLTIP6 = "Zioła";
+UMM_MSI_MARK_TOOLTIP7 = "Surowców";
+UMM_MSI_MARK_TOOLTIP8 = "Run produkcji";
+UMM_MSI_MARK_TOOLTIP9 = "Żywności";
+UMM_MSI_MARK_TOOLTIP10 = "Desery";
+UMM_MSI_MARK_TOOLTIP11 = "Mikstury";
+UMM_MSI_MARK_TOOLTIP12 = "Biały Recipes";
+UMM_MSI_MARK_TOOLTIP13 = "Zielony Recipes";
+UMM_MSI_MARK_TOOLTIP14 = "Niebieski Recipes";
+UMM_MSI_MARK_TOOLTIP15 = "Magenta Recipes";
+UMM_MSI_MARK_TOOLTIP16 = "Gildia wkład";
+UMM_MSI_MARK_TOOLTIPCLICK = "Kliknij aby oznaczyć wszystkie sloty przedmiotów w tej kategorii.";
+
+UMM_MSI_BUTTON_ADDRESSEE = "do:";
+UMM_MSI_BUTTON_RESET = "Zresetować";
+
+UMM_MSI_MAILSTOSEND = "maile do wysyłania: %d";
+UMM_MSI_ADDRESSEE = "do:";
+UMM_MSI_SUBJECT = "temat:";
+UMM_MSI_COD = "PŁATNE PRZY ODBIORZE";
+UMM_MSI_STATUS = "Status:";
+UMM_MSI_STATUS_SENDING = "Wysyłanie...";
+UMM_MSI_STATUS_QUEUED = "W kolejce.";
+UMM_MSI_BUTTON_SEND = "Wyślij";
+UMM_MSI_BUTTON_COD = "PŁATNE PRZY ODBIORZE";
+UMM_MSI_BUTTON_CANCEL = "Anuluj";
+UMM_MSI_SEND_STATUS = "Wysyłanie %d z %d - proszę czekać...";
+UMM_MSI_SEND_MAILBODY = "Hi %s\n\nI używany ostateczny Mail Mod się wysłać ten przedmiot do you.\n\nPlease enjoy.\n\nKind regards\n%s\n\n%s";
+
+--Komunikaty
+UMM_ERROR_CANTSENDSELF = "Użytkownik może nie wysyłać e-maile do siebie.";
+UMM_ERROR_NOSUBJECT = "Proszę wpisać temat na tego maila.";
+UMM_ERROR_CANTSENDBOUND = "Nie można wysłać przedmioty związane.";
+
+UMM_ERROR_AUTOMATIONFAILED = "Automatyzacja procesu nie powiodło się - proces zatrzymany.";
+UMM_ERROR_NOTHINGTAGGED = "Proszę oznaczyć jedną lub więcej wiadomości!";
+UMM_ERROR_CANTDELETE = "Niesłabnący wobec usunąć wiadomości e-mail z załącznikami!";
+UMM_ERROR_CANTRETURN = "Nie wrócić do poczty!";
+UMM_ERROR_CANTTAKECOD = "płatne przy odbiorze wiadomości nie może być zrobione auto!";
+UMM_ERROR_CANTTAKE = "oznaczone poczty nie spełniają warunki wymagane wziąć!";
